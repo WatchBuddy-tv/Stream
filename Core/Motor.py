a04f85e5bc0777567fcc2e233a57c8722077e035
keyiflerolsun <keyiflerolsun@gmail.com>
2026-02-27 03:48:17 +0300

    feat: switch from uvicorn to gunicorn for server execution and adjust worker configuration

--- Core/Motor.py
+++ Core/Motor.py
@@ -3,25 +3,30 @@
 from CLI      import konsol
 from Settings import AYAR, HOST, PORT
 from sys      import version_info
-import uvicorn, subprocess
+import os, uvicorn, subprocess
 
 def basla():
-    surum = f"{version_info[0]}.{version_info[1]}"
-    konsol.print(f"\n[bold gold1]{AYAR['PROJE']}[/] [yellow]:bird:[/] [turquoise2]Python {surum}[/] [bold yellow2]uvicorn[/]", width=70, justify="center")
+    surum   = f"{version_info[0]}.{version_info[1]}"
+    cpu     = os.cpu_count() or 1
+    workers = 2 * cpu + 1
+
+    konsol.print(f"\n[bold gold1]{AYAR['PROJE']}[/] [yellow]:bird:[/] [turquoise2]Python {surum}[/] [bold yellow2]gunicorn × {workers}[/]", width=70, justify="center")
     konsol.print(f"[red]{HOST}[light_coral]:[/]{PORT}[pale_green1] başlatılmıştır...[/]\n", width=70, justify="center")
 
-    uvicorn.run("Core:kekik_FastAPI", host=HOST, port=PORT, proxy_headers=True, forwarded_allow_ips="*", workers=1, log_level="error")
+    # uvicorn.run("Core:kekik_FastAPI", host=HOST, port=PORT, proxy_headers=True, forwarded_allow_ips="*", workers=workers, log_level="error")
 
-    # komut = [
-    #     "gunicorn",
-    #     "-k", "uvicorn.workers.UvicornWorker",
-    #     "Core:kekik_FastAPI",
-    #     "--log-level", "error",
-    #     "--bind", f"{HOST}:{PORT}",
-    #     "--workers", "2",
-    #     "--keep-alive", "5",
-    #     "--worker-tmp-dir", "/dev/shm",
-    #     "--max-requests", "10000", "--max-requests-jitter", "1000"
-    # ]
+    komut = [
+        "gunicorn",
+        "-k", "uvicorn.workers.UvicornWorker",
+        "Core:kekik_FastAPI",
+        "--log-level", "error",
+        "--bind", f"{HOST}:{PORT}",
+        "--workers", str(workers),
+        "--proxy-headers",
+        "--forwarded-allow-ips", "*",
+        "--keep-alive", "5",
+        "--worker-tmp-dir", "/dev/shm",
+        "--max-requests", "10000", "--max-requests-jitter", "1000"
+    ]
 
-    # subprocess.run(komut, check=True)
+    subprocess.run(komut, check=True)
